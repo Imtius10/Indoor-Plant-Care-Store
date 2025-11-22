06c249ca85020871a7e4a0162c50b83030db9932
imtius10 <h.imtius10@gmail.com>
2025-11-23 02:41:51 +0600

    Footer

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const HomeLayout = () => {
     return (
@@ -14,7 +15,7 @@ const HomeLayout = () => {
                     <Outlet></Outlet>
                 </section>
                 <section>
-
+                 <Footer></Footer>
                 </section>
 
             </main>
